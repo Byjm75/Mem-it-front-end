9a7715c6c7308ba4dfdc1dc89a1cba96b7e0db6d
Byjm75 <yoanmark75@gmail.com>
2022-12-12 17:10:43 +0100

    Maj 12/12 premier jetdes  components

---
+++ src/components/Caroussel.tsx
@@ -0,0 +1,107 @@
+import React from "react";
+
+export const Caroussel = () => {
+  return (
+    <div>
+      <div
+        id='carouselExampleCaptions'
+        className='carousel slide'
+        data-bs-ride='false'
+      >
+        <div className='carousel-indicators'>
+          <button
+            type='button'
+            data-bs-target='#carouselExampleCaptions'
+            data-bs-slide-to='0'
+            className='active'
+            aria-current='true'
+            aria-label='Slide 1'
+          ></button>
+          <button
+            type='button'
+            data-bs-target='#carouselExampleCaptions'
+            data-bs-slide-to='1'
+            aria-label='Slide 2'
+          ></button>
+          <button
+            type='button'
+            data-bs-target='#carouselExampleCaptions'
+            data-bs-slide-to='2'
+            aria-label='Slide 3'
+          ></button>
+        </div>
+        <div>
+          <div
+            // style={{ height: "90vh" }}
+            className='carousel-inner'
+          >
+            <div className='carousel-item active'>
+              <img
+                src={require("./Accueil-MEM-IT.jpg")}
+                className='d-block w-100'
+                alt='accueil'
+                style={{ width: "100%", height: "100%" }}
+              />
+              <div className='carousel-caption d-none d-md-block'>
+                <h5>First slide label</h5>
+                <p>
+                  Some representative placeholder content for the first slide.
+                </p>
+              </div>
+            </div>
+            <div className='carousel-item'>
+              <img
+                src={require("./escalade.jpg")}
+                className='d-block w-100'
+                alt="phot de l'escalade"
+              />
+              <div className='carousel-caption d-none d-md-block'>
+                <h5>Second slide label</h5>
+                <p>
+                  Some representative placeholder content for the second slide.
+                </p>
+              </div>
+            </div>
+            <div className='carousel-item'>
+              <img
+                src={require("./sports.jpg")}
+                className='d-block w-100'
+                alt="Louis qui fait de l'escalade"
+              />
+              <div className='carousel-caption d-none d-md-block'>
+                <h5>Third slide label</h5>
+                <p>
+                  Some representative placeholder content for the third slide.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <button
+          className='carousel-control-prev'
+          type='button'
+          data-bs-target='#carouselExampleCaptions'
+          data-bs-slide='prev'
+        >
+          <span
+            className='carousel-control-prev-icon'
+            aria-hidden='true'
+          ></span>
+          <span className='visually-hidden'>Previous</span>
+        </button>
+        <button
+          className='carousel-control-next'
+          type='button'
+          data-bs-target='#carouselExampleCaptions'
+          data-bs-slide='next'
+        >
+          <span
+            className='carousel-control-next-icon'
+            aria-hidden='true'
+          ></span>
+          <span className='visually-hidden'>Next</span>
+        </button>
+      </div>
+    </div>
+  );
+};
---
+++ src/components/Footer.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+
+export const Footer = () => {
+  return (
+    <div>
+      <section className=''>
+        {/*  <!-- Footer -->**/}
+        <footer
+          className='text-center text-white'
+          style={{ backgroundColor: "#9AAEB4" }}
+        >
+          {/*    <!-- Grid container -->**/}
+          <div className='container p-4 pb-0'>
+            {/*      <!-- Section: CTA --> **/}
+            <section className=''>
+              <p className='d-flex justify-content-center align-items-center'>
+                <span className='me-3'>Register for free</span>
+                <button
+                  type='button'
+                  className='btn btn-outline-light btn-rounded'
+                >
+                  Sign up!
+                </button>
+              </p>
+            </section>
+            {/*    <!-- Section: CTA --> **/}
+          </div>
+          {/*   <!-- Grid container --> **/}
+          {/*  <!-- Copyright --> **/}
+          <div
+            className='text-center p-3'
+            style={{ backgroundColor: "rgba(0, 0, 0, 0.2)" }}
+          >
+            © 2020 Copyright:
+            <a className='text-white' href='https://mdbootstrap.com/'>
+              MDBootstrap.com
+            </a>
+          </div>
+          {/*  <!-- Copyright --> **/}
+        </footer>
+        {/* <!-- Footer -->**/}
+      </section>
+    </div>
+  );
+};
