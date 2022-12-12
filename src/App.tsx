f05ec43d748c77511e3265b648be13f208801b0e
badlaand <caron.florian0@gmail.com>
2022-12-13 00:39:12 +0100

    test modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,29 @@
-import React from 'react';
-import Home from './pages/Home';
-import Navbar from './components/Navbar';
-import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React from "react";
+
+import CategoriesForm from "./components/CategoriesForm";
 
 export const App = () => {
   return (
     <div>
+      <img
+        src={require("./assets/JPEG_example_flower.jpg")}
+        alt='logo'
+        width='50em'
+      />
       <BrowserRouter>
-        <Navbar /> Ici un composant directement avec le nom
+        <Navbar />
+        Ici un composant directement avec le nom
         <Routes>
           <Route path='/' element={<Home />} /> Ici une page avec son chemin
         </Routes>
       </BrowserRouter>
+      <div>
+        <CategoriesForm />
+      </div>
     </div>
   );
 };
