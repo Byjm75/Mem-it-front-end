0a2e1a6c1e9cf3e3dd3254c1544b0bdae3ee19f5
MattDao <113097684+MattDao@users.noreply.github.com>
2022-12-15 17:48:17 +0100

    les imports...

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { SignUp } from './pages/SignUp';
-import { SignIn } from './pages/SignIn';
-import { Home } from './pages/Home';
+import { SignUp } from './Pages/SignUp';
+import { SignIn } from './Pages/SignIn';
+import { Home } from './Pages/Home';
 
 
 const App = () => {
