970cc62e1ead5342d00c1a20bb48a4a9deb10cb3
Mouss <elmost777@gmail.com>
2022-12-15 15:26:15 +0100

    signIn, signUp

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { Home } from './Pages/Home';
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { SignUp } from './pages/SignUp';
 import { SignIn } from './pages/SignIn';
+import { Home } from './pages/Home';
 
 
 const App = () => {
