95430628895a805d03b01d2c68e8ac7114cd5d44
Josselin BAPTISTE <josselin.baptiste@sncf.fr>
2022-12-15 15:58:23 +0100

    git pull

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -48,35 +48,35 @@ export const SignUp = () => {
 
   return (
     <div
-      // style={{
-      //   backgroundColor: '#ABCDEF',
-      //   overflow: 'hidden',
-      // }}
+    // style={{
+    //   backgroundColor: '#ABCDEF',
+    //   overflow: 'hidden',
+    // }}
     >
       <Navbar />
       <MDBContainer
-        className="my-2"
+        className='my-2'
         // style={{
         //   height: '30rem',
         // }}
       >
         <MDBCard>
           <MDBRow
-            className="g-0 d-flex align-items-center"
+            className='g-0 d-flex align-items-center'
             style={{
               backgroundColor: 'rgba(180, 200, 200, 0.73)',
             }}
           >
             <MDBCol
-              md="4"
+              md='4'
               style={{
                 height: '29.4rem',
               }}
             >
               <MDBCardImage
-                src="https://mdbootstrap.com/img/new/ecommerce/vertical/004.jpg"
-                alt="phone"
-                className="rounded-t-5 rounded-tr-lg-0"
+                src='https://mdbootstrap.com/img/new/ecommerce/vertical/004.jpg'
+                alt='phone'
+                className='rounded-t-5 rounded-tr-lg-0'
                 fluid
                 style={{
                   height: '29.4rem',
@@ -84,67 +84,67 @@ export const SignUp = () => {
               />
             </MDBCol>
 
-            <MDBCol md="8">
+            <MDBCol md='8'>
               <h1 style={{ textAlign: 'center', color: 'white' }}>
                 Inscrivez vous
               </h1>
               <MDBCardBody
-                className="form-floating mt-1"
+                className='form-floating mt-1'
                 style={{ color: 'white' }}
               >
                 {/* <label htmlFor="pseudo">Pseudo</label> */}
                 <MDBInput
-                  wrapperClass="mb-1"
-                  id="form1"
-                  label="Pseudo"
-                  type="text"
-                  className="form-control"
+                  wrapperClass='mb-1'
+                  id='form1'
+                  label='Pseudo'
+                  type='text'
+                  className='form-control'
                   inputRef={pseudoElement}
-                  placeholder="pseudo"
+                  placeholder='pseudo'
                 />
 
                 <MDBInput
-                  wrapperClass="mb-1"
-                  label="Email address"
-                  id="form2"
-                  type="email"
-                  className="form-control"
-                  placeholder="email@exemple.com"
+                  wrapperClass='mb-1'
+                  label='Email address'
+                  id='form2'
+                  type='email'
+                  className='form-control'
+                  placeholder='email@exemple.com'
                   inputRef={emailElement}
                 />
                 <MDBInput
-                  wrapperClass="mb-1"
-                  label="mot de passe"
-                  id="form2"
-                  type="password"
-                  className="form-control"
-                  placeholder="mot de passe"
+                  wrapperClass='mb-1'
+                  label='mot de passe'
+                  id='form2'
+                  type='password'
+                  className='form-control'
+                  placeholder='mot de passe'
                   inputRef={passwordElement}
                 />
                 <MDBInput
-                  wrapperClass="mb-1"
-                  label="Confirmez mot de passe"
-                  id="form2"
-                  type="password"
-                  className="form-control"
-                  placeholder="confirmez mot de passe"
+                  wrapperClass='mb-1'
+                  label='Confirmez mot de passe'
+                  id='form2'
+                  type='password'
+                  className='form-control'
+                  placeholder='confirmez mot de passe'
                   inputRef={ConfirmPasswordElement}
                 />
 
-                <div className="d-flex justify-content-between mx-1 mb-2">
+                <div className='d-flex justify-content-between mx-1 mb-2'>
                   <MDBCheckbox
-                    name="flexCheck"
-                    value=""
-                    id="flexCheckDefault"
-                    label="Remember me"
+                    name='flexCheck'
+                    value=''
+                    id='flexCheckDefault'
+                    label='Remember me'
                   />
-                  <a href="!#" style={{ color: 'black' }}>
+                  <a href='!#' style={{ color: 'black' }}>
                     Forgot password?
                   </a>
                 </div>
 
                 <MDBBtn
-                  className="mb-2 w-100"
+                  className='mb-2 w-100'
                   style={{ height: '40px' }}
                   onClick={handleSubmitForm}
                 >
