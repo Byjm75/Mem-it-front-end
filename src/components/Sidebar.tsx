1cb682bacf365036d06eaab434d5a7fd8d4ff12c
Josselin BAPTISTE <josselin.baptiste@sncf.fr>
2023-01-06 12:04:46 +0100

    clear token à la déconnexion

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { json } from 'react-router-dom';
 const cdbreact = require('cdbreact');
 const {
   CDBSidebar,
@@ -10,6 +11,23 @@ const {
 } = cdbreact;
 
 export const Sidebar = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  useEffect(() => {
+    const loggedUser = localStorage.getItem('utilisateur');
+    if (loggedUser) {
+      const foundUser = JSON.parse(loggedUser);
+      setEmail(foundUser);
+    }
+  }, []);
+
+  const handleLogout = () => {
+    setEmail('');
+    setPassword('');
+    localStorage.clear();
+  };
+
   return (
     <CDBSidebar
       style={{
@@ -20,30 +38,36 @@ export const Sidebar = () => {
         color: '#806d42',
       }}
     >
-      <CDBSidebarHeader prefix={<i className="fa fa-bars" />}>
-        <a className="navbar-brand" href="/dashboard">
+      <CDBSidebarHeader prefix={<i className='fa fa-bars' />}>
+        <a className='navbar-brand' href='/dashboard'>
           Accueil
         </a>{' '}
       </CDBSidebarHeader>
       <CDBSidebarContent>
         <CDBSidebarMenu>
-          <CDBSidebarMenuItem icon="sticky-note">
-            <a className="navbar-brand" href="/createCategory">Créer une catégorie
+          <CDBSidebarMenuItem icon='sticky-note'>
+            <a className='navbar-brand' href='/createCategory'>
+              Créer une catégorie
             </a>
           </CDBSidebarMenuItem>
-          <CDBSidebarMenuItem icon="sticky-note">
-            <a className="navbar-brand" href="/createTask">
+          <CDBSidebarMenuItem icon='sticky-note'>
+            <a className='navbar-brand' href='/createTask'>
               Créer mémo
             </a>
           </CDBSidebarMenuItem>
-          <CDBSidebarMenuItem icon="th-large">
-            <a className="navbar-brand" href="/categorie">
+          <CDBSidebarMenuItem icon='th-large'>
+            <a className='navbar-brand' href='/categorie'>
               Catégories
             </a>
           </CDBSidebarMenuItem>
-          <CDBSidebarMenuItem icon="credit-card" iconType="solid">
-            <a className="navbar-brand" href="/profil">
+          <CDBSidebarMenuItem icon='credit-card' iconType='solid'>
+            <a className='navbar-brand' href='/profil'>
               Profil
+            </a>
+          </CDBSidebarMenuItem>
+          <CDBSidebarMenuItem icon='credit-card' iconType='solid'>
+            <a className='navbar-brand' href='/admin'>
+              Support Admin
             </a>
           </CDBSidebarMenuItem>
         </CDBSidebarMenu>
@@ -51,10 +75,10 @@ export const Sidebar = () => {
 
       <CDBSidebarFooter style={{ textAlign: 'center' }}>
         <div
-          className="sidebar-btn-wrapper"
+          className='sidebar-btn-wrapper'
           style={{ padding: '20px 5px', fontSize: '12px' }}
         >
-          <a className="navbar-brand" href="/signin">
+          <a className='navbar-brand' href='/signin' onClick={handleLogout}>
             Déconnexion
           </a>
         </div>
@@ -62,5 +86,3 @@ export const Sidebar = () => {
     </CDBSidebar>
   );
 };
-
-;
