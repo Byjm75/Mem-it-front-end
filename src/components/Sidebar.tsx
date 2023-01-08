8addb747365d9eebab5c45904d10ee0f8ca48a97
Mouss <elmost777@gmail.com>
2023-01-08 20:28:50 +0100

    modification des icons sidebar (profil et support admin)

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -38,35 +38,35 @@ export const Sidebar = () => {
         color: '#806d42',
       }}
     >
-      <CDBSidebarHeader prefix={<i className='fa fa-bars' />}>
-        <a className='navbar-brand' href='/dashboard'>
+      <CDBSidebarHeader prefix={<i className="fa fa-bars" />}>
+        <a className="navbar-brand" href="/dashboard">
           Accueil
         </a>{' '}
       </CDBSidebarHeader>
       <CDBSidebarContent>
         <CDBSidebarMenu>
-          <CDBSidebarMenuItem icon='sticky-note'>
-            <a className='navbar-brand' href='/createCategory'>
+          <CDBSidebarMenuItem icon="sticky-note">
+            <a className="navbar-brand" href="/createCategory">
               Créer une catégorie
             </a>
           </CDBSidebarMenuItem>
-          <CDBSidebarMenuItem icon='sticky-note'>
-            <a className='navbar-brand' href='/createTask'>
+          <CDBSidebarMenuItem icon="sticky-note">
+            <a className="navbar-brand" href="/createTask">
               Créer mémo
             </a>
           </CDBSidebarMenuItem>
-          <CDBSidebarMenuItem icon='th-large'>
-            <a className='navbar-brand' href='/categorie'>
+          <CDBSidebarMenuItem icon="th-large">
+            <a className="navbar-brand" href="/categorie">
               Catégories
             </a>
           </CDBSidebarMenuItem>
-          <CDBSidebarMenuItem icon='credit-card' iconType='solid'>
-            <a className='navbar-brand' href='/profil'>
+          <CDBSidebarMenuItem icon="user" iconType="solid">
+            <a className="navbar-brand" href="/profil">
               Profil
             </a>
           </CDBSidebarMenuItem>
-          <CDBSidebarMenuItem icon='credit-card' iconType='solid'>
-            <a className='navbar-brand' href='/admin'>
+          <CDBSidebarMenuItem icon="lock" iconType="solid">
+            <a className="navbar-brand" href="/admin">
               Support Admin
             </a>
           </CDBSidebarMenuItem>
@@ -75,10 +75,10 @@ export const Sidebar = () => {
 
       <CDBSidebarFooter style={{ textAlign: 'center' }}>
         <div
-          className='sidebar-btn-wrapper'
+          className="sidebar-btn-wrapper"
           style={{ padding: '20px 5px', fontSize: '12px' }}
         >
-          <a className='navbar-brand' href='/signin' onClick={handleLogout}>
+          <a className="navbar-brand" href="/signin" onClick={handleLogout}>
             Déconnexion
           </a>
         </div>
