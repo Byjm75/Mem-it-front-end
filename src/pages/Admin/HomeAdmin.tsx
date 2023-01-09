7a0719587391a1cc4e44c1a90dbb05f8329af22b
Josselin BAPTISTE <josselin.baptiste@sncf.fr>
2023-01-09 09:46:38 +0100

    MAJ home admin

--- src/pages/Admin/HomeAdmin.tsx
+++ src/pages/Admin/HomeAdmin.tsx
@@ -1,21 +1,18 @@
-import axios, { AxiosResponse } from 'axios';
 import jwtDecode from 'jwt-decode';
 import Chart from '../../components/Admin/Chart/Chart';
 import ToolsBar from '../../components/ToolsBar';
 import './HomeAdmin.css';
 
- export interface UserData {
-   id: string;
-   pseudo: string;
-   image: string;
-   email: string;
-   password: string;
-   role: string;
- }
+export interface UserData {
+  id: string;
+  pseudo: string;
+  image: string;
+  email: string;
+  password: string;
+  role: string;
+}
 
 const HomeAdmin = () => {
- 
-
   interface DecodTokenType {
     utilisateur: UserData;
     exp: number;
@@ -27,12 +24,10 @@ const HomeAdmin = () => {
     const decodToken: DecodTokenType = jwtDecode(accessToken);
 
     console.log('Token décodé:', decodToken);
-
   }
   return (
     <div>
-      <ToolsBar
-      />
+      <ToolsBar />
       <div className='home'>
         <Chart />
       </div>
