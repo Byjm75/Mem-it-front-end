89875a56b8bfd4729769a038809f153785f644bb
MattDao <113097684+MattDao@users.noreply.github.com>
2023-01-10 21:35:54 +0100

    dasboard carré

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,7 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-// import { Home } from './Pages/Home';
-// import Profil from './Pages/Profil';
-import { SignUp } from './Pages/SignUp';
-import { SignIn } from './Pages/SignIn';
-import HomeAdmin from './Pages/Admin/HomeAdmin';
-import UserAdmin from './Pages/Admin/UserAdmin';
-import Dashboard from './Pages/Dashboard';
-import Categorie from './Pages/Categorie';
-import CreateCategory from './Pages/CreateCategory';
-import CreateTask from './Pages/CreateTask';
-import { Home } from './Pages/Home';
-import Profil from './Pages/Profil';
-import Memos from './Pages/Memos';
+
 import { SignUp } from './Pages/SignUp';
 import { SignIn } from './Pages/SignIn';
 import HomeAdmin from './Pages/Admin/HomeAdmin';
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -9,18 +9,26 @@ import FooterConnect from '../components/FooterConnect';
 const Dashboard = () => {
   return (
     <div style={{ height: '100vh' }}>
+      <div
+        style={{
+          width: '100%',
+          position: 'sticky',
+          top: '0',
+          overflow: 'hidden',
+          zIndex: '1',
+        }}
+      >
+        <ToolsBar />
+      </div>{' '}
+      <div>
         <div
           style={{
-            width: '100%',
-            position: 'sticky',
-            top: '0',
+            display: 'flex',
+            position: 'fixed',
             overflow: 'hidden',
             zIndex: '1',
           }}
         >
-          <ToolsBar />
-        </div>{' '}
-        <div>
           <div
             style={{
               display: 'flex',
@@ -29,78 +37,76 @@ const Dashboard = () => {
               zIndex: '1',
             }}
           >
-           
-            
-              <h1
-                style={{
-                  width: '100%',
-                  position: 'relative',
-                  display: 'flex',
-                  justifyContent: 'end',
-                  alignItems: 'flex-end',
-                  margin: '10px 0 0 ',
-
-                  color: '#007872',
-                  fontWeight: 'bold',
-                }}
-              >
-                Mon espace
-              </h1>
-            </div>
-            <hr />
-
-            <div
-              className="card-tools row"
-              style={{
-                display: 'flex',
-                justifyContent: 'space-around',
-              }}
-            >
-              <CardPlus />
-              <CardFavoris />
-            </div>
-            <div>
-              <h2
-                style={{
-                  width: '100%',
-                  position: 'relative',
-                  display: 'flex',
-                  justifyContent: 'end',
-                  alignItems: 'flex-end',
-                  margin: '10px 0 0 ',
-
-                  color: '#007872',
-                  fontWeight: 'bold',
-                }}
-              >
-                Derniers ajoûts{' '}
-              </h2>
-
-              <hr />
-
-              
-              <div
-                className="card-tools row"
-                style={{ display: 'flex', justifyContent: 'space-around' }}
-              ></div>
-            </div>
-            <div style={{ position: 'fixed', right: '15px', bottom: '120px' }}>
-              <AddBtn />
-            </div>
+            <Sidebar />
           </div>
-          <div
+
+          <h1
             style={{
-              marginTop: '30px',
-              position: 'fixed',
-              bottom: '0',
               width: '100%',
+              position: 'relative',
+              display: 'flex',
+              justifyContent: 'end',
+              alignItems: 'flex-end',
+              margin: '10px 0 0 ',
+
+              color: '#007872',
+              fontWeight: 'bold',
             }}
           >
-            <FooterConnect />
-          </div>
+            Mon espace
+          </h1>
         </div>
-      
-  
+        <hr />
+
+        <div
+          className="card-tools row"
+          style={{
+            display: 'flex',
+            justifyContent: 'space-around',
+          }}
+        >
+          <CardPlus />
+          <CardFavoris />
+        </div>
+        <div>
+          <h2
+            style={{
+              width: '100%',
+              position: 'relative',
+              display: 'flex',
+              justifyContent: 'end',
+              alignItems: 'flex-end',
+              margin: '10px 0 0 ',
+
+              color: '#007872',
+              fontWeight: 'bold',
+            }}
+          >
+            Derniers ajoûts{' '}
+          </h2>
+
+          <hr />
+
+          <div
+            className="card-tools row"
+            style={{ display: 'flex', justifyContent: 'space-around' }}
+          ></div>
+        </div>
+        <div style={{ position: 'fixed', right: '15px', bottom: '120px' }}>
+          <AddBtn />
+        </div>
+      </div>
+      <div
+        style={{
+          marginTop: '30px',
+          position: 'fixed',
+          bottom: '0',
+          width: '100%',
+        }}
+      >
+        <FooterConnect />
+      </div>
+    </div>
   );
 };
 
