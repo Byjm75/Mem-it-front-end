c4eaaa841d97e5e64d2ff199542512a590369215
Mouss <elmost777@gmail.com>
2023-01-12 14:06:19 +0100

    footerConnect fixed , page memos et composant einterface

---
+++ src/interface/Interface.ts
@@ -0,0 +1,55 @@
+export interface MemosProps {
+  id: string;
+  title: string;
+  date_event: Date;
+  body: string;
+  image: string;
+  url: string;
+  date_creation: string;
+}
+
+export interface DecodTokenType {
+  utilisateur: UserData;
+  exp: number;
+  iat: number;
+}
+
+export interface Categories {
+  id: string;
+  title: string;
+  image: string;
+  favoris: string;
+}
+
+export interface CardcategoryProps {
+  categoryAffich: Categories;
+}
+
+export interface CardmemoProps {
+  memoAffich: MemosProps;
+}
+
+export interface DropdownPropsMemo {
+  memo: MemosProps;
+}
+
+export interface DropdownPropsCat {
+  category: Categories;
+}
+
+export interface TableUsers {
+  users: UserData;
+}
+
+export interface UserData {
+  id: string;
+  pseudo: string;
+  image: string;
+  email: string;
+  password: string;
+  role: string;
+}
+
+export interface SearchBarProps {
+  onSearch: (userInput: string) => void;
+}
